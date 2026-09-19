94ac0a042e153c8772b6edbad0c42b888db6d5c0
techbyshar <techbyshar.net@gmail.com>
2026-09-19 09:34:46 -0500

    Update config.js

--- config.js
+++ config.js
@@ -1,3 +1,3 @@
 window.TECHBYSHAR_FORM_CONFIG = {
-  endpoint: "https://script.google.com/macros/s/AKfycbxRQiA6NY86tIs7sML5Pg_vxjoSoNWiYQC9x93_-bY/dev"
+  endpoint: "https://script.google.com/macros/s/AKfycbw2mhcpCVrAo1C5e3TYr4C0j_y_SIuoZ71ghps3OLqBQfzIFnFJUt7E_mpZmDHNNPHz/exec"
 };
